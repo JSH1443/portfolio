c1a127ba785bbc4e278e9351506a416a1bb5b8d9
JSH1443 <inv1443@gmail.com>
2022-06-19 03:08:19 -0700

    구조수정 완료[초기시작]

--- AVR/Fan_Project/main.c
+++ AVR/Fan_Project/main.c
@@ -1,90 +1,42 @@
-//sw1 = PD2
-//sw2 = PD3
-//bldc = PB5
-//servo = PE3
-//temp = PF0
-//photocell = PF1
-//led = PE0
-//lcd = PA0~1, PC0~7
-
 
 #define F_CPU                           16000000UL
 
 #include <avr/io.h>
 #include <util/delay.h>
-#include <avr/interrupt.h>
-#include <stdbool.h>
 
-volatile int sw1_flag=0;
-volatile int sw2_flag=0;
-int sw1=0;
-int sw2=0;
-
-void sw_init (void)
-{
-
-    EICRA |= (1 << ISC21);
-    //EICRA |= (1 << ISC20);
-    EICRA |= (1 << ISC31);
-    //EICRA |= (1 << ISC30);
-
-    EIMSK |= (1 << INT2);
-    EIMSK |= (1 << INT3);
-
-
-    sei();
-}
-
+#include "uart.h"
+#include "lcd.h"
+#include "switch.h"
+#include "bldc.h"
+#include "servo.h"
+#include "temp.h"
+#include "phtocell.h"
+#include "bluetooht.h"
 
 
 
 int main(void)
 {
     uart_init();
-    sw_init();
+    b_uart_init();
     lcd_init();
+    sw_init();
+    BLDC_init();
+    servo_init();
+    temp_init();
+    photocell_init();
 
     uart_string_trans("Select Mode\n");
     lcd_write_string("Select Mode\n");
 
-  while(1)
-  {
-     if(sw1_flag==true)
-    {
-        sw1++;
-
-        if(sw1>4)
-        {
-         sw1=0;
-        }
-
-        switch_mode(sw1, sw2);
-        sw1_flag=false;
-    }
-
-    if(sw2_flag==true)
-    {
-        sw2++;
-
-        if(sw2>3)
-        {
-         sw2=0;
-        }
-
-      switch_mode(sw1, sw2);
-      sw2_flag=false;
-    }
-
+   while(1)
+   {
+    bluetooth_mode();
+    switch_mode();
+    _delay_ms(50);
    }
- }
 
-ISR(INT2_vect)
-{
-    sw1_flag = true;
 }
 
-ISR(INT3_vect)
-{
-    sw2_flag = true;
-}
+
 
