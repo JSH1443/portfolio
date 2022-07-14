d424b2e26c28bd12a0e109f8d4b61e5ba5010334
JSH1443 <inv1443@gmail.com>
2022-07-14 00:34:37 -0700

    bluetooth mode define 수정완료 [BLUETOOTH]

--- AVR/Fan_Project/bluetooth.c
+++ AVR/Fan_Project/bluetooth.c
@@ -6,76 +6,82 @@
 #include "bluetooht.h"
 #include "bldc.h"
 #include "servo.h"
+#include "switch.h"
 
 
-#define b_manual_mode         1
-#define b_auto_mode           2
-#define b_servo_start         3
-#define b_servo_stop          4
-#define b_low                 5
-#define b_middle              6
-#define b_high                7
-#define b_stop                8
-#define b_reset               9
+#define B_MANUAL_MODE         1
+#define B_AUTO_MODE           2
+#define B_SERVO_START         3
+#define B_SERVO_STOP          4
+#define B_LOW                 5
+#define B_MIDDLE              6
+#define B_HIGH                7
+#define B_STOP                8
+#define b_RESET               9
+#define RESET                 0
+
+#define CLOCKWISE               25
+#define COUNTERCLOCKWISE        5
+
+#define MANUAL_MODE             1
+#define AUTO_MODE               1
+
+#define ANGULAR_SPEED_DUTY      0.1
 
 
-volatile int bluetooth_rx_data;
-int b_manual_mode_select=0;
-int b_auto_mode_select=0;
+volatile int bluetooth_data;
 float b_duty = 5.0;
 
 ISR(USART3_RX_vect)
 {
-    bluetooth_rx_data = UDR3;
+    bluetooth_data = UDR3;
 }
 
 void bluetooth_mode(void)
 {
-  if(bluetooth_rx_data == b_manual_mode)
+  if(bluetooth_data == B_MANUAL_MODE)
     {
+     uart_string_trans("Manual Mode\n");
+     lcd_write_string("Manual Mode\n");
+    }
 
-     b_manual_mode_select =1;
-     b_auto_mode_select =0;
-     b_uart_string_trans("Manual Mode\n");
-
-        if(bluetooth_rx_data == b_low)
+        if(bluetooth_data == B_LOW)
         {
            bldc_low();
         }
 
-        if(bluetooth_rx_data == b_middle)
+        if(bluetooth_data == B_MIDDLE)
         {
            bldc_middle();
         }
 
-        if(bluetooth_rx_data==b_high)
+        if(bluetooth_data==B_HIGH)
         {
             bldc_high();
         }
 
-        if(bluetooth_rx_data==b_stop)
+        if(bluetooth_data==B_STOP)
         {
            bldc_stop();
         }
-    }
 
-    else if (bluetooth_rx_data == b_auto_mode)
+
+    if (bluetooth_data == B_AUTO_MODE)
     {
-        b_auto_mode_select =1;
-        b_manual_mode_select =0;
-        b_uart_string_trans("Auto Mode\n");
+        uart_string_trans("Auto Mode\n");
+        lcd_write_string("Auto Mode\n");
         temperature_sensor();
         photocell_mode();
     }
 
-       if(bluetooth_rx_data==b_servo_start)
+       if(bluetooth_data==B_SERVO_START)
          {
-            b_duty +=0.1;
+            b_duty +=ANGULAR_SPEED_DUTY;
             servo_counter_clockwise(b_duty);
 
-            if(b_duty>25.0)
+            if(b_duty>CLOCKWISE)
             {
-             for(b_duty=25.0; b_duty>5.0; b_duty-=0.1)
+             for(b_duty=CLOCKWISE; b_duty>COUNTERCLOCKWISE; b_duty-=ANGULAR_SPEED_DUTY)
              {
                 servo_clockwise(b_duty);
              }
@@ -83,18 +89,16 @@ void bluetooth_mode(void)
             }
          }
 
-         if(bluetooth_rx_data==b_servo_stop)
+         if(bluetooth_data==B_SERVO_STOP)
          {
           servo_stop();
          }
 
 
-      if(bluetooth_rx_data==b_reset && (b_manual_mode_select==1 || b_auto_mode_select==1))
+      if(bluetooth_data==b_RESET)
         {
-            b_auto_mode_select=0;
-            b_manual_mode_select=0;
-            b_uart_string_trans("Select Mode\n");
+            bluetooth_data=RESET;
+            reset_mode();
         }
 
 }
-
